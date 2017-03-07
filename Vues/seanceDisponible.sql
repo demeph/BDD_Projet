27c4358983b80ee45fe805db285f1ed51d99e222
Clement JEHANNO <E148703J@I012V7pc02.ensinfo.sciences.univ-nantes.prive>
2017-03-07 16:56:05 +0100

    vues seanceDispo v2

--- Vues/seanceDisponible.sql
+++ Vues/seanceDisponible.sql
@@ -1,14 +1,14 @@
 DROP VIEW seanceDisponible;
 
 CREATE VIEW seanceDisponible AS
-SELECT idSeance
-FROM Seance NATURAL JOIN Salle
+SELECT nomFilm, dateProjection, franchise, numSalle
+FROM Seance NATURAL JOIN Film NATURAL JOIN Cinema NATURAL JOIN Salle
 WHERE dateProjection >= (SELECT sysdate FROM dual)
       AND
       (nbPlaceHandicape + nbPlaceStandard + nbPlaceDbox) > (SELECT SUM(nbPlaceHandicapeRes + nbPlaceStandardRes + nbPlaceDboxRes) 
 							    FROM Reservation NATURAL JOIN Seance
 							    WHERE dateProjection >= (SELECT sysdate FROM dual)
 							    GROUP BY idSeance)
-GROUP BY idSeance;
+GROUP BY nomFilm, dateProjection, franchise, numSalle;
        
 
