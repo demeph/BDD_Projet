265bf0a2b80be45ba81d20bda19bd86945309f29
mooss <mooss@protonmail.com>
2017-03-08 15:34:35 +0100

    jeu de test Client

---
+++ JeuxDessais/client_bad.sql
@@ -0,0 +1,9 @@
+INSERT INTO L3_35.Client Values (75, 'jehanno', 'clement');
+
+UPDATE L3_35.Acteur
+SET prenomA = 'henry'
+WHERE idAct = 1;
+
+DELETE * FROM L3_35.Film;
+
+SELECT * FROM Salle;
---
+++ JeuxDessais/client_ok.sql
@@ -0,0 +1,5 @@
+SELECT * FROM L3_35.Casting
+WHERE idFilm = 1;
+
+SELECT * FROM L3_35.Realisateur
+WHERE idReal = 1;
